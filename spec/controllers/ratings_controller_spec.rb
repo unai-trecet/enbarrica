ab72113e2d1a80196ca724bfba6e2e94c27adcb1
Unai Trecet <unai.trecet@gmail.com>
2015-04-04 20:04:27 +0200

    Ratings controller spec and controller created.

---
+++ spec/controllers/ratings_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe RatingsController do 
+  describe "GET update" do
+    it "sets the @rating varible with the id passed by" do
+      
+    end
+    
+    it "updates rating's valoracion"
+  end  
+end
