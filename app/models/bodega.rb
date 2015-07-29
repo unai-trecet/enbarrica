0599218633431aad01f82c56f2482fcf7c393361
Unai Trecet <unai.trecet@gmail.com>
2015-07-29 17:00:55 +0200

    average_rating method created for Bodega model. Spec also done.

--- app/models/bodega.rb
+++ app/models/bodega.rb
@@ -2,4 +2,18 @@ class Bodega < ActiveRecord::Base
   has_many :vinos
   belongs_to :denominacion
   has_many :comentarios, as: :comentable
+
+  def average_rating
+    suma_rating = 0
+    contador = 0
+
+    vinos.each do |vino|
+      if vino.average_rating.present? 
+        suma_rating += vino.average_rating
+        contador += 1
+      end
+    end
+
+    suma_rating / contador
+  end
 end
--- spec/models/bodega_spec.rb
+++ spec/models/bodega_spec.rb
@@ -5,4 +5,27 @@ describe Bodega do
   it { should have_many(:vinos) }
   it { should belong_to(:denominacion) }
   it { should have_many(:comentarios) }
+
+  describe "average_rating" do
+    it "returns the average rating of all the bodega's vinos" do
+      finca_estacada = Fabricate :bodega
+
+      estacada_reserva = Fabricate :vino, bodega_id: finca_estacada.id
+      estacada_gran_reserva = Fabricate :vino, bodega_id: finca_estacada.id
+
+      ana = Fabricate :user
+      jose = Fabricate :user
+      marta = Fabricate :user
+      
+      anas_rating_estacada_reserva = Fabricate :rating, user_id: ana.id, vino_id: estacada_reserva.id, valoracion: 7
+      joses_rating_estacada_reserva = Fabricate :rating, user_id: jose.id, vino_id: estacada_reserva.id, valoracion: 10
+      martas_rating_estacada_reserva = Fabricate :rating, user_id: marta.id, vino_id: estacada_reserva.id, valoracion: 4
+
+      anas_rating_estacada_gran_reserva = Fabricate :rating, user_id: ana.id, vino_id: estacada_gran_reserva.id, valoracion: 9
+      joses_rating_estacada_gran_reserva = Fabricate :rating, user_id: jose.id, vino_id: estacada_gran_reserva.id, valoracion: 9
+      martas_rating_estacada_gran_reserva = Fabricate :rating, user_id: marta.id, vino_id: estacada_gran_reserva.id, valoracion: 3
+      
+      expect(finca_estacada.average_rating).to eq(7)
+    end
+  end
 end
