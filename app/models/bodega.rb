5ef7a25248108952d182b4824c402b29fa5be94d
Unai Trecet <unai.trecet@gmail.com>
2015-07-29 17:13:11 +0200

    number_of_rated_vinos added to Bodega model. Spec done.

--- app/models/bodega.rb
+++ app/models/bodega.rb
@@ -3,17 +3,29 @@ class Bodega < ActiveRecord::Base
   belongs_to :denominacion
   has_many :comentarios, as: :comentable
 
-  def average_rating
-    suma_rating = 0
+  def number_of_rated_vinos
     contador = 0
 
     vinos.each do |vino|
       if vino.average_rating.present? 
-        suma_rating += vino.average_rating
         contador += 1
       end
     end
 
-    suma_rating / contador
+    contador
   end
+
+  def average_rating
+    suma_rating = 0
+
+    vinos.each do |vino|
+      if vino.average_rating.present? 
+        suma_rating += vino.average_rating
+      end
+    end
+
+    suma_rating / number_of_rated_vinos
+  end
+
+
 end
--- spec/models/bodega_spec.rb
+++ spec/models/bodega_spec.rb
@@ -6,10 +6,9 @@ describe Bodega do
   it { should belong_to(:denominacion) }
   it { should have_many(:comentarios) }
 
-  describe "average_rating" do
-    it "returns the average rating of all the bodega's vinos" do
-      finca_estacada = Fabricate :bodega
-
+  context "comentarios and rating" do
+    let(:finca_estacada) { Fabricate :bodega }
+    before do
       estacada_reserva = Fabricate :vino, bodega_id: finca_estacada.id
       estacada_gran_reserva = Fabricate :vino, bodega_id: finca_estacada.id
 
@@ -24,8 +23,18 @@ describe Bodega do
       anas_rating_estacada_gran_reserva = Fabricate :rating, user_id: ana.id, vino_id: estacada_gran_reserva.id, valoracion: 9
       joses_rating_estacada_gran_reserva = Fabricate :rating, user_id: jose.id, vino_id: estacada_gran_reserva.id, valoracion: 9
       martas_rating_estacada_gran_reserva = Fabricate :rating, user_id: marta.id, vino_id: estacada_gran_reserva.id, valoracion: 3
-      
-      expect(finca_estacada.average_rating).to eq(7)
+    end    
+
+    describe "number_of_rated_vinos" do
+      it "returns the number of rated vinos belonged by the bodega" do
+        expect(finca_estacada.number_of_rated_vinos).to eq(2)
+      end
+    end
+    
+    describe "average_rating" do
+      it "returns the average rating of all the bodega's vinos" do        
+        expect(finca_estacada.average_rating).to eq(7)
+      end
     end
   end
 end
