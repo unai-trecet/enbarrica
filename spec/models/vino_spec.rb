16bab64fb63203d21f71b4c9c91d971b95e62123
Unai Trecet <unai.trecet@gmail.com>
2015-07-29 14:22:29 +0200

    Vino model average_rating spec added.

--- spec/models/vino_spec.rb
+++ spec/models/vino_spec.rb
@@ -11,6 +11,17 @@ describe Vino do
   it { should have_many(:comentarios) }
 
   describe "average_rating" do
-    it "returns the average valoracion of every rating done for this vino"
+    it "returns the average valoracion of every rating done for this vino" do
+      estacada = Fabricate :vino
+      ana = Fabricate :user
+      jose = Fabricate :user
+      marta = Fabricate :user
+      
+      anas_rating = Fabricate :rating, user_id: ana.id, vino_id: estacada.id, valoracion: 7
+      joses_rating = Fabricate :rating, user_id: jose.id, vino_id: estacada.id, valoracion: 10
+      martas_rating = Fabricate :rating, user_id: marta.id, vino_id: estacada.id, valoracion: 4
+
+      expect(estacada.average_rating).to eq(7)
+    end
   end
 end
