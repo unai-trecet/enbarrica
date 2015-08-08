2dac439b721092fa37be6bfb6a04d82f5680f479
Unai Trecet <unai.trecet@gmail.com>
2015-08-08 18:44:11 +0200

    Working on how to change user's rating trespassing everithing to rates.js

--- app/assets/javascripts/rates.js
+++ app/assets/javascripts/rates.js
@@ -3,39 +3,41 @@ $(document).ready(function() {
     readOnly: true,
     score: $('#vino_rating').data('valoracion'),
     path: '../assets/'
+  });  
+
+  $('#valoracion_usuario').raty({
+    readOnly: true,
+    score: $('#valoracion_usuario').data('valoracion'),
+    path: '../assets/'
   }); 
-}); 
 
-$('#valoracion_usuario').raty({
-  readOnly: true,
-  score: $('#vino_rating').data('valoracion'),
-  path: '../assets/'
-}); 
+  $('.btn-default').on('click', function() {
+    var rating_id = $('#boton_cambiar_valoracion').data('rating_id');
+    alert(rating_id);
+    $('#boton_cambiar_valoracion').raty({
+        score: '0',
+        path: "../assets",
+        click: function(score, evt) {
+          $.ajax({
+            url: "/ratings/" + rating_id,
+            type: 'PATCH',
+            data: { valoracion: score, rating_id: rating_id } 
+          });
+      }              
+    });
+  });
 
-$('.btn-default').on('click', function() {
-  $('#boton_cambiar_valoracion').raty({
+  $('.btn-default').on('click', function(){
+    $('#boton_valoracion').raty({
       score: $('#vino_rating').data('valoracion'),
-      path: "../assets",
+      path: '../assets',
       click: function(score, evt) {
         $.ajax({
-          url: "/ratings/" + $('#vino_rating').data('rating_id'),
-          type: 'PATCH',
-          data: { valoracion: score, rating_id: $('#vino_rating').data('rating_id') } 
+          url: "/ratings",
+          type: 'POST',
+          data: { rating: { valoracion: score, vino_id: $('#vino_rating').data('vino_id'), user_id: "#{ current_user.id }"} } 
         });
-    }              
-  }).slideToggle();
-});
-
-$('.btn-default').on('click', function(){
-  $('#boton_valoracion').raty({
-    score: $('#vino_rating').data('valoracion'),
-    path: '../assets',
-    click: function(score, evt) {
-      $.ajax({
-        url: "/ratings",
-        type: 'POST',
-        data: { rating: { valoracion: score, vino_id: $('#vino_rating').data('vino_id'), user_id: "#{ current_user.id }"} } 
-      });
-    }              
-  }).slideToggle();
+      }              
+    }).slideToggle();
+  });
 });
