f94fc3fa07d6fb72a5856c76ba85572a0b87f7a8
Unai Trecet <unai.trecet@gmail.com>
2015-08-08 16:48:53 +0200

    Some changes showing comments in vino's show page.

--- spec/models/comentario_spec.rb
+++ spec/models/comentario_spec.rb
@@ -8,4 +8,5 @@ describe Comentario do
   it { should validate_presence_of(:comentable_id) }
   it { should validate_presence_of(:comentable_type) }
   it { should validate_presence_of(:user_id) }
+  
 end
