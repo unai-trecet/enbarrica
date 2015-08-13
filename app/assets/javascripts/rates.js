c7126f73aedc2722d9a55316b725db56033b6988
Unai Trecet <unai.trecet@gmail.com>
2015-08-13 13:01:57 +0200

    Bodega show page rating passed to rate.js

--- app/assets/javascripts/rates.js
+++ app/assets/javascripts/rates.js
@@ -26,8 +26,6 @@ $(document).ready(function() {
     }).slideToggle();
   });
 
-
-/////////////////////////////////////////////
   $('.btn-default').on('click', function(){
     $('#boton_valoracion').raty({
       score: "0",
@@ -40,21 +38,11 @@ $(document).ready(function() {
         });
       }              
     }).slideToggle();
+  });
+
+  $('#valoracion_bodega ').raty({
+    readOnly: true,  
+    score: $('#valoracion_bodega').data('valoracion'),
+    path: '../assets'
   }); 
-
-
-  // $('.btn-default').on('click', function(){
-
-  //   $('#boton_valoracion').raty({
-  //     score: $('#vino_rating').data('valoracion'),
-  //     path: '../assets',
-  //     click: function(score, evt) {
-  //       $.ajax({
-  //         url: "/ratings",
-  //         type: 'POST',
-  //         data: { rating: { valoracion: score, vino_id: $('#vino_rating').data('vino_id'), user_id: "#{ current_user.id }"} } 
-  //       });
-  //     }              
-  //   }).slideToggle();
-  // });
 });
--- app/controllers/bodegas_controller.rb
+++ app/controllers/bodegas_controller.rb
@@ -1,6 +1,7 @@
 class BodegasController < ApplicationController
   def show
     @bodega = Bodega.find(params[:id])
+    @comentarios = @bodega.comentarios
     @comentario = Comentario.new
   end
 end
