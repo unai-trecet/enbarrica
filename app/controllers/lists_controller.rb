7bbaf545b0a7211ea8d4964314970adba7cb3302
Unai Trecet <unai.trecet@gmail.com>
2015-08-26 13:40:01 +0200

    List creation from user's show page done: lists controller action, spec, new and create js files.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,29 @@
 class ListsController < ApplicationController
-  def new
+  respond_to :html, :js
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        flash[:notice] = "Lista creada con éxito"
+        format.js { render inline: "location.reload()" }
+        format.html { redirect_to user_path(current_user) }
+      else
+        flash[:error] = "Tiene que darle un nombre a la lista"
+        format.js { render inline: "location.reload()" }
+        format.html { render current_user }
+      end
+    end  
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:nombre, :user_id)
   end
 end
--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -7,8 +7,43 @@ describe ListsController do
   end
 
   describe 'GET new' do
-
+    
+    it 'sets @list variable' do
+      xhr :get, :new
+      expect(assigns :list).to be_instance_of List
+    end
   end
 
+  describe 'POST create' do
+    context 'with valid input' do
+      
+      before do
+        ana = Fabricate :user
+        xhr :post, :create, list: { user_id: ana.id, nombre: "Lista test" }
+      end
 
+      it 'creates a list' do
+        expect(List.count).to eq(1)
+      end
+
+      it 'set notice message' do
+        expect(flash[:notice]).to be_present        
+      end
+    end
+
+    context 'with invalid input'
+      
+      before do
+        ana = Fabricate :user
+        xhr :post, :create, list: { user_id: ana.id }
+      end
+
+      it 'does not create a list' do
+        expect(List.count).to eq(0)
+      end
+
+      it 'sets the error' do
+        expect(flash[:error]).to be_present
+      end   
+  end 
 end
